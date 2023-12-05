1c4ebc63f1e09c5cd61e79aff7271faff6094ffd
osamaaleed <152627308+osamaaleed@users.noreply.github.com>
2023-12-05 10:28:12 +0300

    first commit

---
+++ ss.js
@@ -0,0 +1,10 @@
+onload=function(){
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
+}
